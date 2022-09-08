b8fcdc134c6c0e14ea244739ed694ca781c4a3ae
Yefimkin Denys <iefimkindenis@gmail.com>
2022-09-08 18:45:11 +0300

    update task 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory'
 
 import user from 'data/user.json'
 import data from 'data/data.json'
 import friends from 'data/friends.json'
+import transactions from 'data/transactions.json'
 
 
 
@@ -29,6 +31,7 @@ export const App = () => {
       />
       <Statistics title='Upload Stats' stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
 
     </div>
   );
